7e35834ee5286dd4a55fbdc5d156f1494687c3c6
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-13 16:17:47 +0100

    Add files via upload

---
+++ Fanuc/C code/codegen/dll/EulerBasis/rtGetNaN.h
@@ -0,0 +1,23 @@
+/* 
+ * File: rtGetNaN.h 
+ *  
+ * MATLAB Coder version            : 2.6 
+ * C/C++ source code generated on  : 01-May-2015 14:43:34 
+ */
+
+#ifndef __RTGETNAN_H__
+#define __RTGETNAN_H__
+
+#include <stddef.h>
+#include "rtwtypes.h"
+#include "rt_nonfinite.h"
+
+extern real_T rtGetNaN(void);
+extern real32_T rtGetNaNF(void);
+
+#endif
+/* 
+ * File trailer for rtGetNaN.h 
+ *  
+ * [EOF] 
+ */
