ea872737e3e46a601b324f85c221eccadc87fac9
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-14 10:52:47 +0100

    Add files via upload

---
+++ Fanuc/slprj/_sfprj/DynVerify/_self/sfun/src/DynVerify_sfun.h
@@ -0,0 +1,33 @@
+#ifndef __DynVerify_sfun_h__
+#define __DynVerify_sfun_h__
+
+/* Include files */
+#define S_FUNCTION_NAME                sf_sfun
+#include "sfc_sf.h"
+#include "sfc_mex.h"
+#include "rtwtypes.h"
+#include "sfcdebug.h"
+#define rtInf                          (mxGetInf())
+#define rtMinusInf                     (-(mxGetInf()))
+#define rtNaN                          (mxGetNaN())
+#define rtIsNaN(X)                     ((int)mxIsNaN(X))
+#define rtIsInf(X)                     ((int)mxIsInf(X))
+
+/* Type Definitions */
+
+/* Named Constants */
+#define CALL_EVENT                     (-1)
+
+/* Variable Declarations */
+extern int32_T _sfEvent_;
+extern uint32_T _DynVerifyMachineNumber_;
+extern real_T _sfTime_;
+
+/* Variable Definitions */
+
+/* Function Declarations */
+extern void DynVerify_initializer(void);
+extern void DynVerify_terminator(void);
+
+/* Function Definitions */
+#endif
